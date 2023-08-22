2a3622740cd409d1e96788c2ced82726c1402acb
Annab3flores <142801460+Annab3flores@users.noreply.github.com>
2023-08-22 17:03:41 -0300

    Add files via upload

---
+++ ac1.py
@@ -0,0 +1,35 @@
+"""
+
+Aluna: Anna Beatriz R D Flores
+Curso: Análise e Desenvolvimento de Sistema
+
+"""
+
+# Exercício 1: 
+
+a = float(input("insira o valor: "))
+b = float(input("insira o segundo valor: "))
+c = float(input("insira o terceiro valor: "))
+
+delta= b**2 -4*a*c
+Raiz1 = (-b+delta**0.5) / (2*a)
+Raiz2 = (-b-delta**0.5) / (2*a)
+
+print("Raiz1", Raiz1)
+print("Raiz2", Raiz2)
+
+# Exercício 2: 
+
+ano = int(input("informe o ano: "))
+print(ano%4 == 0 and ano % 100!=(not ano % 400!=0))
+
+      
+
+
+
+
+
+
+
+
+
